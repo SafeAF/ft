36844034d246f21c2e6ef6a9c954783d7ea8bf16
safeaf <transiencymail@gmail.com>
2023-06-15 09:43:33 -0600

    added validations to company model

--- web/app/models/company.rb
+++ web/app/models/company.rb
@@ -1,14 +1,17 @@
 class Company < ApplicationRecord
-    belongs_to :user
+  belongs_to :user
+  has_rich_text :description
+  has_one_attached :logo do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
+  
+  # Validation
+  validates :name, :contact, :address, :phone, :about, :category, presence: true
+  validates :name, :email, uniqueness: true
+  validates :phone, format: { with: /\A\d+\z/, message: "Integer only. No sign allowed." }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 
-    has_rich_text :description
-
-    has_one_attached :logo do |attachable|
-        attachable.variant :thumb, resize_to_limit: [100, 100]
-      end
-
-
-    def self.ransackable_attributes(auth_object = nil)
-      ["about", "address", "category", "contact", "email", "hours", "id", "name", "phone", "website"]
-    end
+  def self.ransackable_attributes(auth_object = nil)
+    ["about", "address", "category", "contact", "email", "hours", "id", "name", "phone", "website"]
+  end
 end
