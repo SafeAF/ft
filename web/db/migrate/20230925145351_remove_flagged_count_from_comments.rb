09abe001ca0e6fbd4d2cbc2c83d1b1cf7f36eece
SafeAF <transiencymail@gmail.com>
2023-09-25 08:55:22 -0600

    removed redundant flagged_count from comments

---
+++ web/db/migrate/20230925145351_remove_flagged_count_from_comments.rb
@@ -0,0 +1,5 @@
+class RemoveFlaggedCountFromComments < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :comments, :flagged_count, :integer
+  end
+end
--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_24_172450) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_25_145351) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -70,7 +70,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_24_172450) do
     t.integer "commentable_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "flagged_count", default: 0
     t.integer "flags_count", default: 0
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
     t.index ["user_id"], name: "index_comments_on_user_id"
