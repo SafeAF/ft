2e7e52eee8a9987570ca0b6575caa9b6a9f91521
SafeAF <transiencymail@gmail.com>
2023-09-30 07:23:48 -0600

    DM models

--- web/app/models/conversation.rb
+++ web/app/models/conversation.rb
@@ -1,4 +1,7 @@
 class Conversation < ApplicationRecord
-  belongs_to :sender
-  belongs_to :recipient
+  belongs_to :sender, class_name: 'User'
+  belongs_to :recipient, class_name: 'User'
+  
+  has_many :messages, dependent: :destroy
 end
+
--- web/app/models/user.rb
+++ web/app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
+  # Validations
+  validates :username, presence: true, uniqueness: true
+
+  # Relationships
   has_many :companies, dependent: :destroy
   has_many :listings, dependent: :destroy
   has_many :comments, dependent: :destroy
@@ -14,8 +19,17 @@ class User < ApplicationRecord
 
   has_rich_text :bio
 
-  
-  validates :username, presence: true, uniqueness: true
+  # Direct Messages
+  has_many :sent_conversations, class_name: 'Conversation', foreign_key: :sender_id, dependent: :destroy
+  has_many :received_conversations, class_name: 'Conversation', foreign_key: :recipient_id, dependent: :destroy
+
+  has_many :messages, dependent: :destroy
+
+  # For convenience, you might want to define a method to get all conversations for a user
+  def conversations
+    Conversation.where("sender_id = ? OR recipient_id = ?", self.id, self.id)
+  end
+
 
 
 end
