15f161bed62c340d0385953ce8cd29c8d0bbad8e
SafeAF <transiencymail@gmail.com>
2023-10-02 09:13:07 -0600

    avatar displayed on profile, comments, replies

--- web/app/controllers/users_controller.rb
+++ web/app/controllers/users_controller.rb
@@ -99,7 +99,7 @@ class UsersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:bio, :avatar, :username, :email, :password)
+    params.require(:user).permit(:bio, :avatar)
   end
 
 
