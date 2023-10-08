640b7873d11af5b9c28c0f6ac6594158ec3d9cae
SafeAF <transiencymail@gmail.com>
2023-10-08 07:53:27 -0600

    added exception to auth in co cont for index show

--- web/app/controllers/companies_controller.rb
+++ web/app/controllers/companies_controller.rb
@@ -1,5 +1,5 @@
 class CompaniesController < 
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: %i[index show]
   before_action :set_company, only: %i[ show edit update destroy ]
   before_action :check_user, only: [:edit, :update, :destroy]
 
