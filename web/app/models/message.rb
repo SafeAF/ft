bccb28f7469ccd9d98cabea76ffb9861e2a1b53d
SafeAF <transiencymail@gmail.com>
2023-10-09 10:08:34 -0600

    added notifications to priv message

--- web/app/models/message.rb
+++ web/app/models/message.rb
@@ -7,5 +7,19 @@ class Message < ApplicationRecord
   def message_time
     created_at.strftime("%m/%d/%y at %l:%M %p")
   end
+
+  # Callback to create a notification after a message is saved
+  after_create :create_notification
+
+  def create_notification
+    if self.user
+      Notification.create(
+        user: self.user,
+        notifiable: self,
+        status: :unread,
+        message: "#{self.user.username} sent you a private message."
+      )
+    end
+  end
 end
 
