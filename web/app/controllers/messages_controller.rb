a18a24ad260fb01ce803ad5c7537272af74bd1b3
SafeAF <transiencymail@gmail.com>
2023-10-09 08:45:58 -0600

    message submission working

--- web/app/controllers/messages_controller.rb
+++ web/app/controllers/messages_controller.rb
@@ -7,19 +7,34 @@ class MessagesController < ApplicationController
     @message = Message.new
   end
 
+  # def create
+  #   @message = Message.new(message_params)
+  #   @message.user_id = current_user.id
+  #   if @message.save
+  #     respond_to do |format|
+  #       format.turbo_stream
+  #     end
+
+  #   else
+  #     render 'index'
+  #   end
+  # end
+
+
   def create
     @message = Message.new(message_params)
     @message.user_id = current_user.id
+    @message.conversation_id = @conversation.id
+  
     if @message.save
-      respond_to do |format|
-        format.turbo_stream
-      end
-
+      redirect_to conversation_path(@conversation)
     else
+      Rails.logger.debug(@message.errors.full_messages.to_sentence)
+      @messages = @conversation.messages
       render 'index'
     end
   end
-
+  
   
   private
 
