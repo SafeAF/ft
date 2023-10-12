f563db8d8ee0ed0e0005555c6df011523ea2e3ff
SafeAF <transiencymail@gmail.com>
2023-10-12 10:31:34 -0600

    paginating noti

--- web/app/controllers/notifications_controller.rb
+++ web/app/controllers/notifications_controller.rb
@@ -1,11 +1,10 @@
 class NotificationsController < ApplicationController
     before_action :authenticate_user!
   
-    # Display the current user's unread notifications
-    ## Not being used as of yet. See users_controller#notifications
     def index
-        @notifications = current_user.notifications.unread.order(created_at: :desc)
+        @notifications = current_user.notifications.unread.order(created_at: :desc).page(params[:page]).per(10)
     end
+    
   
     # Mark a notification as read      
     def mark_as_read
