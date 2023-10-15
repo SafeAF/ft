24a20a0384febbb607aca65352b323c097a596c4
SafeAF <transiencymail@gmail.com>
2023-10-15 11:47:45 -0600

    username validations

--- web/app/models/user.rb
+++ web/app/models/user.rb
@@ -4,8 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # Validations
-  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  # Username validations
+  validates :username,
+  presence: true,
+  length: { minimum: 3, maximum: 20 },
+  exclusion: { in: %w(admin root guest superuser), message: "is reserved." },
+  uniqueness: { case_sensitive: false },
+  format: { with: /\A[a-zA-Z0-9]+\Z/, message: "only allows alphanumeric characters" }
+
+
 
   # Relationships
   has_many :companies, dependent: :destroy
