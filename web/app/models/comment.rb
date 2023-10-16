be8d6204991d213eeffcdfed62ebf97c3c6c4c77
SafeAF <transiencymail@gmail.com>
2023-10-16 11:14:01 -0600

    comment on profile noti rephrased to sound better

--- web/app/models/comment.rb
+++ web/app/models/comment.rb
@@ -9,6 +9,7 @@ class Comment < ApplicationRecord
 
   # Callback to create a notification after a comment is saved
   after_create :create_notification
+
   def create_notification
     # Determine the owner of the commentable item
     owner = if self.commentable.respond_to?(:user)
@@ -18,14 +19,18 @@ class Comment < ApplicationRecord
             end
   
     if owner
+      commentable_type = self.commentable.class.name.downcase
+      # Special case for when the commentable is a User
+      commentable_type = "profile" if commentable_type == "user"
+  
       Notification.create(
         user: owner,
         notifiable: self,
         status: :unread,
-        message: "#{self.user.username} commented on your #{self.commentable.class.name.downcase}."
+        message: "#{self.user.username} commented on your #{commentable_type}."
       )
     end
   end
-  
+    
 end
 
