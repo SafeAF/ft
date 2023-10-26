3b4d4fdc80a50cc7adb1eff4cab3913f2b423355
SafeAF <transiencymail@gmail.com>
2023-10-26 11:27:20 -0600

    access control on messages controller

--- web/app/controllers/messages_controller.rb
+++ web/app/controllers/messages_controller.rb
@@ -1,12 +1,12 @@
 class MessagesController < ApplicationController
   before_action :set_conversation, only: [:create]
+  before_action :check_participation, only: [:create]
+
   def create
     @message = Message.new(message_params)
     @message.user_id = current_user.id
     @message.conversation_id = params[:conversation_id]
-    @conversation = Conversation.find(params[:conversation_id])
   
-    # Redundant but oh well.
     if @message.save
       redirect_to conversation_path(@conversation)
     else
@@ -14,16 +14,19 @@ class MessagesController < ApplicationController
     end
   end
   
-  
-  
   private
 
   def set_conversation
     @conversation = Conversation.find(params[:conversation_id])
+  end
+  
+  def check_participation
+    unless @conversation.sender_id == current_user.id || @conversation.recipient_id == current_user.id
+      redirect_to conversations_path, alert: 'You do not have permission to access this conversation.'
+    end
   end
 
   def message_params
     params.require(:message).permit(:content, :conversation_id)
   end
 end
-
