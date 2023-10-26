8d5688467c1f9fe44c1ac4aefe2ff41ae1480b69
SafeAF <transiencymail@gmail.com>
2023-10-26 11:57:05 -0600

    added auth to timeline controller

--- web/app/controllers/timelines_controller.rb
+++ web/app/controllers/timelines_controller.rb
@@ -1,6 +1,7 @@
 class TimelinesController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     @poasts = current_user.timeline_poasts.where(visible: true).order(created_at: :desc).page(params[:page]).per(5)
   end  
 end
-  
