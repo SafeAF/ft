218e58c9173da8a8a960f32b5cbe4e2efe8d2f0c
SafeAF <transiencymail@gmail.com>
2023-10-26 11:36:12 -0600

    access control on relationships controller

--- web/app/controllers/relationships_controller.rb
+++ web/app/controllers/relationships_controller.rb
@@ -1,16 +1,33 @@
 class RelationshipsController < ApplicationController
-    before_action :authenticate_user!
-  
-    def create
-      user = User.find(params[:followed_id])
-      current_user.follow(user)
-      redirect_to user
-    end
-  
-    def destroy
-      user = Relationship.find(params[:id]).followed
-      current_user.unfollow(user)
-      redirect_to user
+  before_action :authenticate_user!
+  before_action :set_followed_user, only: [:create]
+  before_action :set_relationship, only: [:destroy]
+
+  def create
+    if @followed_user
+      current_user.follow(@followed_user)
+      redirect_to @followed_user
+    else
+      redirect_to root_path, alert: 'User not found.'
     end
   end
-  
+
+  def destroy
+    if @relationship && @relationship.follower == current_user
+      current_user.unfollow(@relationship.followed)
+      redirect_to @relationship.followed
+    else
+      redirect_to root_path, alert: 'You do not have permission to unfollow this user.'
+    end
+  end
+
+  private
+
+  def set_followed_user
+    @followed_user = User.find_by(id: params[:followed_id])
+  end
+
+  def set_relationship
+    @relationship = Relationship.find_by(id: params[:id])
+  end
+end
