34836861fdfc35637886ba206f4547fb625cfc15
SafeAF <transiencymail@gmail.com>
2023-11-01 07:45:04 -0600

    dont create notification for user if they message another user

--- web/app/models/message.rb
+++ web/app/models/message.rb
@@ -8,22 +8,31 @@ class Message < ApplicationRecord
   validates_presence_of :content, :conversation_id, :user_id
 
   
+  def recipient
+    if self.user_id == self.conversation.sender_id
+      User.find(self.conversation.recipient_id)
+    else
+      User.find(self.conversation.sender_id)
+    end
+  end
+
   # Callback to create a notification after a message is saved
   after_create :create_notification
 
   def create_notification
-    
-
-    if self.user
+    # Assuming the `recipient` method returns the user who is supposed to receive the message
+    recipient = self.recipient
+  
+    if recipient && self.user != recipient
       Notification.create(
-        user: self.user,
+        user: recipient,  # Note that the notification now targets the recipient, not the sender
         notifiable: self,
         status: :unread,
         message: "#{self.user.username} sent you a private message."
       )
     end
   end
-
+  
   def message_time
     created_at.strftime("%m/%d/%y at %l:%M %p")
   end
