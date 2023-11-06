40b0b6ce5bc0626fd7c4d9fafca5eb230d8692ab
SafeAF <transiencymail@gmail.com>
2023-11-06 10:08:52 -0700

    testing edit to convo migration file

--- web/db/migrate/20230930131748_create_conversations.rb
+++ web/db/migrate/20230930131748_create_conversations.rb
@@ -1,8 +1,8 @@
 class CreateConversations < ActiveRecord::Migration[7.0]
   def change
     create_table :conversations do |t|
-      t.references :sender, null: false, foreign_key: true
-      t.references :recipient, null: false, foreign_key: true
+      t.references :sender, null: false, foreign_key: { to_table: :users}
+      t.references :recipient, null: false, foreign_key: { to_table: :users}
 
       t.timestamps
     end
