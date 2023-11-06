17b1d8515ae51d0038ecca45d4b5322485d29878
SafeAF <transiencymail@gmail.com>
2023-11-06 08:13:28 -0700

    testing

--- web/db/migrate/20231106150855_fix_recipient_foreign_key_in_conversations.rb
+++ web/db/migrate/20231106150855_fix_recipient_foreign_key_in_conversations.rb
@@ -2,7 +2,7 @@ class FixRecipientForeignKeyInConversations < ActiveRecord::Migration[7.0]
   def change
     # First, remove the incorrect foreign key if it exists
     remove_foreign_key :conversations, :recipients, if_exists: true
-    remove_foreign_key :conversations, :senders, if_exists: true
+   # remove_foreign_key :conversations, :senders, if_exists: true
 
     # Now, add the correct foreign key pointing to the `users` table
     add_foreign_key :conversations, :users, column: :recipient_id
