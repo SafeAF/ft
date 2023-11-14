f6c64c156e8afb6dfd73150cc824d7b255651c75
SafeAF <transiencymail@gmail.com>
2023-11-14 06:02:57 -0700

    relationship validations

--- web/app/models/relationship.rb
+++ web/app/models/relationship.rb
@@ -2,8 +2,6 @@ class Relationship < ApplicationRecord
     belongs_to :follower, class_name: 'User'
     belongs_to :followed, class_name: 'User'
   
-    validates :follower_id, presence: true
-    validates :followed_id, presence: true
 
     after_create :create_notification
 
@@ -14,6 +12,17 @@ class Relationship < ApplicationRecord
         status: :unread,
         message: "#{self.follower.username} started following you."
       )
+    end
+
+    validates :follower_id, presence: true
+    validates :followed_id, presence: true
+    validates :follower_id, uniqueness: { scope: :followed_id }
+    validate :follower_cannot_be_followed
+  
+    private
+  
+    def follower_cannot_be_followed
+      errors.add(:follower_id, "can't follow themselves") if follower_id == followed_id
     end
   end
   
