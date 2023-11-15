68dd07fecfd4e2e46ed2ff7702a367bf67557d34
SafeAF <transiencymail@gmail.com>
2023-11-15 07:25:04 -0700

    commented out remove flagged_count comments migration as in production deployment that column doesn't exist

--- web/db/migrate/20230925145351_remove_flagged_count_from_comments.rb
+++ web/db/migrate/20230925145351_remove_flagged_count_from_comments.rb
@@ -1,5 +1,5 @@
 class RemoveFlaggedCountFromComments < ActiveRecord::Migration[7.0]
   def change
-    remove_column :comments, :flagged_count, :integer
+    # remove_column :comments, :flagged_count, :integer
   end
 end
