809ade393fa43772f3e1a0941d44589ba4b27fca
SafeAF <transiencymail@gmail.com>
2023-11-15 07:29:26 -0700

    same but for articles

--- web/db/migrate/20231011154639_remove_flagged_count_from_articles.rb
+++ web/db/migrate/20231011154639_remove_flagged_count_from_articles.rb
@@ -1,5 +1,5 @@
 class RemoveFlaggedCountFromArticles < ActiveRecord::Migration[7.0]
   def change
-    remove_column :articles, :flagged_count, :integer
+    #remove_column :articles, :flagged_count, :integer
   end
 end
