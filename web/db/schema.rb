d7d5702c4cf2600fcc0bfe167c7726b8162d7afe
SK <transiencymail@gmail.com>
2023-12-04 06:33:55 -0700

    added bannedip table

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_03_141014) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_04_133227) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -100,6 +100,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_03_141014) do
     t.string "name", null: false
     t.string "description"
     t.string "image_path"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "banned_ips", force: :cascade do |t|
+    t.string "ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
