2d79db7230eecfacc4dd9b85b0a2c53187b593b3
SK <transiencymail@gmail.com>
2023-12-04 14:24:06 -0700

    added missing comments count to timeline show action

--- web/app/controllers/timelines_controller.rb
+++ web/app/controllers/timelines_controller.rb
@@ -3,8 +3,18 @@ class TimelinesController < ApplicationController
 
 
   def show
-    @poasts = current_user.timeline_poasts.where(visible: true).order(created_at: :desc).page(params[:page]).per(5)
+    @poasts = current_user.timeline_poasts.where(visible: true)
+               .left_joins(:comments)
+               .select('poasts.*, COUNT(comments.id) AS comments_count')
+               .group('poasts.id')
+               .order(created_at: :desc)
+               .page(params[:page]).per(5)
     @most_followed_users = User.who_to_follow(current_user)
   end  
+
+  # def show
+  #   @poasts = current_user.timeline_poasts.where(visible: true).order(created_at: :desc).page(params[:page]).per(5)
+  #   @most_followed_users = User.who_to_follow(current_user)
+  # end  
 end
  
